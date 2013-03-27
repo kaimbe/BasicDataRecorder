74b5446b83a7aff3e5eaa6c3122edead9106f3de
kaimbe <matt.newell.web@gmail.com>
2013-03-27 13:45:58 -0230

    fix admin & user servlets

--- proj/src/servlets/User.java
+++ proj/src/servlets/User.java
@@ -1,25 +1,17 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSession;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.util.Enumeration;
-import java.net.URLDecoder;
-
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-
-import db.UserAuthDB;
-import java.sql.SQLException;
 
 import com.google.gson.Gson;
+
+import db.BPRMException;
 
 
 public class User extends HttpServlet{
@@ -38,9 +30,11 @@ public class User extends HttpServlet{
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         html.printHtmlStart(out);
+        out.println("<body>");
         
         // body goes here
         
+        out.println("</body>");
         html.printHtmlEnd(out);
     }
 	
